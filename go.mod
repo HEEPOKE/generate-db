e2e1988a70b19e3f64a0771c71d3b46374db095c
HEEPOKE <77528292+HEEPOKE@users.noreply.github.com>
2023-07-23 00:39:13 +0700

    :money_with_wings: update config environment

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/HEEPOKE/generate-db
 
 go 1.20
+
+require github.com/joho/godotenv v1.5.1
--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,1 +1,48 @@
 package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+var (
+	Cfg *Config
+)
+
+type Config struct {
+	MYSQL_DATABASE         string
+	MYSQL_USER             string
+	MYSQL_ROOT_PASSWORD    string
+	MYSQL_HOST_PORT        string
+	MYSQL_CONTAINER_PORT   string
+	REDIS_URL              string
+	REDIS_PORT             string
+	REDIS_PASSWORD         string
+	REDIS_REPLICATION_MODE string
+}
+
+func LoadConfig() (*Config, error) {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+		return nil, err
+	}
+
+	config := &Config{
+		MYSQL_DATABASE:         os.Getenv("MYSQL_DATABASE"),
+		MYSQL_USER:             os.Getenv("MYSQL_USER"),
+		MYSQL_ROOT_PASSWORD:    os.Getenv("MYSQL_ROOT_PASSWORD"),
+		MYSQL_HOST_PORT:        os.Getenv("MYSQL_HOST_PORT"),
+		MYSQL_CONTAINER_PORT:   os.Getenv("MYSQL_CONTAINER_PORT"),
+		REDIS_URL:              os.Getenv("REDIS_URL"),
+		REDIS_PORT:             os.Getenv("REDIS_PORT"),
+		REDIS_PASSWORD:         os.Getenv("REDIS_PASSWORD"),
+		REDIS_REPLICATION_MODE: os.Getenv("REDIS_REPLICATION_MODE"),
+	}
+
+	Cfg = config
+
+	return config, nil
+}
