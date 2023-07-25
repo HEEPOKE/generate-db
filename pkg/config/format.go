d1c9ed424c753d845453071f1f02bd93c1267306
HEEPOKE <77528292+HEEPOKE@users.noreply.github.com>
2023-07-25 14:34:39 +0700

    :money_with_wings: add format

---
+++ pkg/config/format.go
@@ -0,0 +1,8 @@
+package config
+
+const (
+	UppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	LowercaseLetters = "abcdefghijklmnopqrstuvwxyz"
+	Numbers          = "0123456789"
+	SpecialChars     = "!@#$%^&*()-_=+,.?/:;{}[]~"
+)
